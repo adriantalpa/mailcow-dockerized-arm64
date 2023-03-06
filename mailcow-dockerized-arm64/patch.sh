dd1f8d655aff6b9d7bbe282bd094da2a8b0260c5
Adrian <adriantalpa@gmail.com>
2023-03-06 21:43:40 +0000

    Fix: correct backup image name sub

--- mailcow-dockerized-arm64/patch.sh
+++ mailcow-dockerized-arm64/patch.sh
@@ -11,7 +11,7 @@ cp -r Dockerfiles/ ${MAILCOW_INSTALL_DIR}/data/
 cp ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh
 #Build a local backup image and update the backup script
 docker build -t backup ${MAILCOW_INSTALL_DIR}/data/Dockerfiles/backup/ 
-awk '{sub("mailcow/backup","backup:latest"); print}' ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh > ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh
+awk '{sub("mailcow/backup:latest","backup:latest"); print}' ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh > ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh
 
 echo -e "\n::: 1. Launch the build using: docker-compose -f docker-compose.override.yml build inside $MAILCOW_INSTALL_DIR"
 echo -e "::: 2. Run generate_config.sh"
