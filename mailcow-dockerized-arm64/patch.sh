1a3dff852a68515bd63a3706c579896567caf42f
Adrian <adriantalpa@gmail.com>
2023-03-06 20:58:12 +0000

    Update patching process

--- mailcow-dockerized-arm64/patch.sh
+++ mailcow-dockerized-arm64/patch.sh
@@ -4,9 +4,15 @@ MAILCOW_INSTALL_DIR="/opt/mailcow-dockerized"
 
 # Download and overwrite docker-compose.override.yml if it exists
 
-wget -O ${MAILCOW_INSTALL_DIR}/docker-compose.override.yml https://raw.githubusercontent.com/maxtroughear/docker-images/main/mailcow-dockerized-arm64/docker-compose.override.yml
-
-# Set the DEBIAN_DOCKER_IMAGE variable in ./helper-scripts/backup_and_restore.sh to multi-arch image
+cp ${MAILCOW_INSTALL_DIR}/helper-scripts/docker-compose.override.yml.d/BUILD_FLAGS/docker-compose.override.yml ${MAILCOW_INSTALL_DIR}
+cp docker-compose.yml ${MAILCOW_INSTALL_DIR}
+cp -r Dockerfiles/ ${MAILCOW_INSTALL_DIR}/data/
 
 cp ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh
-awk '{sub("mailcow/backup","ghcr.io/maxtroughear/mailcow-backup"); print}' ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh > ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh
+#Build a local backup image and update the backup script
+docker build -t backup ${MAILCOW_INSTALL_DIR}/data/Dockerfiles/backup/ 
+awk '{sub("mailcow/backup","backup:latest"); print}' ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.original.sh > ${MAILCOW_INSTALL_DIR}/helper-scripts/backup_and_restore.sh
+
+echo -e "\n::: 1. Launch the build using: docker-compose -f docker-compose.override.yml build inside $MAILCOW_INSTALL_DIR"
+echo -e "::: 2. Run generate_config.sh"
+echo -e "::: 3. Bring up the stack as normal: docker-compose up -d"
